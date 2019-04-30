1693e37a64d63321d96dc3c9d3e15b6387002e85
Caleb Crumley <30866648+crumleyc@users.noreply.github.com>
2019-04-30 15:18:08 -0400

    final version push

--- src/MainDriver.java
+++ src/MainDriver.java
@@ -9,36 +9,60 @@ public class MainDriver {
 		String playerID2 = "";
 		String firstnName = "";
 		String lastName = "";
+		String teamName = "";
+		String teamYear = "";
 		DBHelper dao = new DBHelper();
-		
+
 		while (command != 'q') {
 			System.out.println("Use one of the following commands to execute a predefined query or to quit the program: ");
 			System.out.println("(a) Find player's best season and compare BA to historical average");
 			System.out.println("(b) Show the top 10 average homerun stats for players above and below average weight.");
+			System.out.println("(c) World Series analysis");
+			System.out.println("(d) MVP analysis");
+			System.out.println("(s) Search for player's playerID by last name.");
+			System.out.println("(t) Search based on team name and year");
 			System.out.println("(q) Quit the program");
 			System.out.print("\nEnter command here: ");
 			command = keyboard.next().charAt(0);
-			
+
 			// Process user input
 			switch (command)
 			{
 				case 'a':
-					System.out.print("Enter a playerID: ");
+				  System.out.print("Enter a playerID: ");
 					playerID1 = keyboard.next();
 					dao.getHistoricalAvgComparison(playerID1);
 					break;
 				case 'b':
 					dao.getHomerunWeightAnalysis();
 					break;
+				case 'c':
+					dao.getWorldSeriesAnalysis();
+					break;
+				case 'd':
+					dao.getMVPAnalysis();
+					break;
+				case 's':
+					System.out.println("Enter player's last name: ");
+					lastName = keyboard.next();
+					dao.getPlayerID(lastName);
+					break;
+				case 't':
+					System.out.print("Enter a team name: ");
+					teamName = keyboard.next();
+					System.out.print("Enter a year: ");
+					teamYear = keyboard.next();
+					dao.getTeamInfo(teamName, teamYear);
+					break;
 				case 'q':
 					System.out.println("Exiting program...");
 					keyboard.close();
 					break;
-					
+
 				default:
 					System.out.println(command + " is an invalid command.");
 			}
-			
+
 		}
 	}
 
